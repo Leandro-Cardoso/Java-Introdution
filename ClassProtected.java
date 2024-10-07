9db71a569a372126a9851a2f73b38ba02eae5dda
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-07 01:18:35 -0300

    Protected

---
+++ ClassProtected.java
@@ -0,0 +1,10 @@
+public class ClassProtected {
+    /*
+     * Metodos e atributos "protected" só podem ser usados dentro do mesmo pacote.
+     */
+    protected String test;
+
+    public ClassProtected() {
+        this.test = "Passou";
+    }
+}
--- PublicPrivate.java
+++ PublicPrivate.java
@@ -1,18 +1,11 @@
 public class PublicPrivate {
-    public static void main(String[] args) {
-        /*
-         * Metodos ou atributos publicos podem ser usados em qualquer classe, já os privados, apenas podem ser usados dentro da classe que eles pertencem.
-         */
-        InnerPublicPrivate obj1 = new InnerPublicPrivate(10, "Mesa");
-        System.out.println("\n " + obj1.getNum() + "\n");
-    }
-}
-
-public class InnerPublicPrivate {
+    /*
+     * Metodos ou atributos publicos podem ser usados em qualquer classe, já os privados, apenas podem ser usados dentro da classe que eles pertencem.
+     */
     private int num;
     String name;
 
-    public InnerPublicPrivate(int num, String name) {
+    public PublicPrivate(int num, String name) {
         this.num = num;
         this.name = name;
     }

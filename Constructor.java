bedd6b69f37e964b2030f368f41ef2ae1ef79472
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-07 15:14:41 -0300

    Referencia this

---
+++ Constructor.java
@@ -0,0 +1,27 @@
+public class Constructor {
+    public static void main(String[] args) {
+        /*
+         * "this" pode invocar a propria classe tambem e passar os parametros como padrão dentro de um metodo construtor sem parametros.
+         */
+        Dog dog1 = new Dog();
+        System.out.println(dog1.str());
+    }
+}
+
+class Dog {
+    String name;
+    int age;
+
+    public Dog() {
+        this("Rex", 0); // Valor padrão para o metodo construtor.
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String str() {
+        return "\n Nome: " + name + "\n Idade: " + age + "\n";
+    }
+}

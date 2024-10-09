00cd6202de4302f1af55454c098e745992c92920
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-09 16:23:56 -0300

    Divisão inteira

--- Java02ArithmeticOperator.java
+++ Java02ArithmeticOperator.java
@@ -7,5 +7,14 @@ public class Java02ArithmeticOperator {
         int b = 2 * a;
         
         System.out.println("Resultado = " + (a + b)); // Concatenação seguida de uma soma de A e B
+
+        // Divisão inteira:
+        int x = 5;
+        int y = 2;
+        System.out.println("\nDivisão inteira: " + x + " / " + y + " = " + (x / y));
+
+        // Divisão normal:
+        double z = 2;
+        System.out.println("Divisão inteira: " + x + " / " + z + " = " + (x / z) + "\n");
     }
 }

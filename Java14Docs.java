7852eec730f561de08909b5150aae32113f4baa7
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-09 00:01:05 -0300

    Java Docs

---
+++ Java14Docs.java
@@ -0,0 +1,18 @@
+/**
+ * Esse {@code Java Docs} serve para descrever caso necessario uma classe ou um metodo. E precisa ser escrito acima da definição da classe ou metodo.
+ */
+
+public class Java14Docs {
+    /**
+     * Descrição de um metodo de exemplo.
+     * Abaixo uma lista:
+     * <ul>
+     * <li><b>Item 1</b></li>
+     * <li><b>Item 2</b></li>
+     * <li><b>Item 3</b></li>
+     * </ul>
+     */
+    
+    public void metodo() {
+    }
+}

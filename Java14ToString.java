d59ddfb8107a0c9e9f2aba3f1cb06379305bdf6f
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-15 15:23:13 -0300

    Metodo toString

---
+++ Java14ToString.java
@@ -0,0 +1,19 @@
+public class Java14ToString {
+    public static void main(String[] args) {
+        /*
+         * Todos os objetos sao filhos da classe Object do Java, entao e possivel sobreescrever a descricao da classe sobreescrevendo o metodo "toString()".
+         */
+
+        Test test = new Test();
+        System.out.println(test);
+    }
+}
+
+class Test {
+    private String name = "Teste";
+
+    @Override
+    public String toString() {
+        return "\n Classe: Test\n Nome: " + this.name + "\n";
+    }
+}

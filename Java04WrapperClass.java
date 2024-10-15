a66751fa00dbaa4d86128740f06191c73a9b20cf
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-15 18:54:32 -0300

    Estrutura switch

--- Java04WrapperClass.java
+++ Java04WrapperClass.java
@@ -1,9 +1,11 @@
 public class Java04WrapperClass {
     public static void main(String[] args) {
         /*
-        Classes Wrapper:
-        Oferece funcionalidades, metodos proprios. Já os tipos primitivos não.
-        */
+         * Classes Wrapper:
+         * Oferece funcionalidades, metodos proprios. Já os tipos primitivos não.
+         * 
+         * Buscar usar sempre os metodos ja existentes.
+         */
         Byte idade = 35;
         Integer codigo = 1353540;
         Double preco = 250.90;
--- Java08Conditional.java
+++ Java08Conditional.java
@@ -1,20 +1,60 @@
+import java.util.Scanner;
+
 public class Java08Conditional {
     public static void main(String[] args) {
         /*
-         * Estrutura if else:
+         * ESTRUTURA IF:
          * if (condicao) {executa}
          * else if (condicao) {executa}
          * else {executa}
          * 
-         * Estrutura ternaria:
+         * ESTRUTURA TERNARIA:
          * (condicao) ? verdadeiro : falso
          * 
-         * E totalmente possivel encadear condicionais.
+         * OBS: E totalmente possivel encadear condicionais.
          * 
-         * Estrutura de verificacao de condicionais:
+         * ESTRUTURA DE VERIFICACAO DE CONDICAO:
          * (condicao) && (condicao) -> As duas e verdadeira?
          * (condicao) || (condicao) -> Uma das duas e verdadeira?
          * !(condicao) -> A condicao nao e verdadeira?
+         * 
+         * ESTRUTURA SWITCH:
+         * switch (var) {
+         *      case valor:
+         *          executar
+         *          break;
+         *      default:
+         *          executar
+         *          break;
+         * }
          */
+
+        Scanner scanner = new Scanner(System.in);
+        int num;
+
+        System.out.println("\n Digite um numero de 1 a 5: ");
+        num = scanner.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("\n Voce digitou: 1");
+                break;
+            case 2:
+                System.out.println("\n Voce digitou: 2");
+                break;
+            case 3:
+                System.out.println("\n Voce digitou: 3");
+                break;
+            case 4:
+                System.out.println("\n Voce digitou: 4");
+                break;
+            case 5:
+                System.out.println("\n Voce digitou: 5");
+                break;
+            default:
+                System.out.println("\n Opcao invalida !");
+                break;
+        }
+        System.out.println();
     }
 }

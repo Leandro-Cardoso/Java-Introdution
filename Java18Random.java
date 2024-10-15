ceca70f7b2116891a15f49223415064518957100
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-10-15 17:29:26 -0300

    Biblioteca Random

---
+++ Java18Random.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class Java18Random {
+    public static void main(String[] args) {
+        /*
+         * Biblioteca Random excelente para gerar numeros aleatorios, mas não possui seguranca.
+         */
+
+        Random random = new Random();
+        int range = 10;
+        int n = 10;
+
+        int num;
+        float numF;
+
+        System.out.println("\n INTEIROS:");
+
+        for (int i = 0; i < n; i++) {
+            num = random.nextInt(range) + 1;
+            System.out.println("\n Numero gerado: " + num);
+        }
+        
+        System.out.println("\n FLUTUANTES:");
+
+        for (int i = 0; i < n; i++) {
+            numF = random.nextFloat() * range;
+            System.out.println("\n Numero gerado: " + numF);
+        }
+        
+        System.out.println();
+    }
+}
